89d2b9e604d79c34d466242a34580c788f1091d3
Duc <671299@sv.vnua.edu.cn>
2025-04-07 15:09:46 +0700

    giao diện trắc nghiệm

--- app/src/main/java/com/example/englishapp/MainActivity.java
+++ app/src/main/java/com/example/englishapp/MainActivity.java
@@ -2,20 +2,29 @@ package com.example.englishapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    CardView cardQuiz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        addView();
+    }
 
+    // ánh xạ views từ xml
+    private void addView() {
+        cardQuiz =(CardView) findViewById(R.id.card_quiz);
+    }
 
-
+    public void onClickQuiz(View view) {
+        Intent intent = new Intent(this, BaiKT.class);
+        startActivity(intent);
     }
 }
